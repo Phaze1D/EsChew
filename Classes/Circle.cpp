edc9569fcf28053afe53b961c4003dbfb60bfc5a
David Villarreal <steadypathapp@gmail.com>
2015-03-05 12:17:56 -0600

    Circle Class Added

---
+++ Classes/Circle.cpp
@@ -0,0 +1,9 @@
+//
+//  Circle.cpp
+//  SQ
+//
+//  Created by David Villarreal on 3/5/15.
+//
+//
+
+#include "Circle.h"
---
+++ Classes/Circle.h
@@ -0,0 +1,14 @@
+//
+//  Circle.h
+//  SQ
+//
+//  Created by David Villarreal on 3/5/15.
+//
+//
+
+#ifndef __SQ__Circle__
+#define __SQ__Circle__
+
+#include <stdio.h>
+
+#endif /* defined(__SQ__Circle__) */
