c5a2bb522e9aeb8acd00fd1c88153af18441313b
David Villarreal <steadypathapp@gmail.com>
2015-03-09 10:13:55 -0600

    StarPower Class Added 	Added StarPower Class

---
+++ Classes/StarPower.cpp
@@ -0,0 +1,11 @@
+//
+//  StarPower.cpp
+//  SQ
+//
+//  Created by David Villarreal on 3/9/15.
+//
+//
+
+#include "StarPower.h"
+
+USING_NS_CC;
---
+++ Classes/StarPower.h
@@ -0,0 +1,22 @@
+//
+//  StarPower.h
+//  SQ
+//
+//  Created by David Villarreal on 3/9/15.
+//
+//
+
+#ifndef __SQ__StarPower__
+#define __SQ__StarPower__
+
+#include <stdio.h>
+#include "cocos2d.h"
+
+class StarPower: public cocos2d::Sprite  {
+    
+    
+public:
+    
+};
+
+#endif /* defined(__SQ__StarPower__) */
